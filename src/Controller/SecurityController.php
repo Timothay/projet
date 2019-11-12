84dd300d8afd9a476728991653b3d541078d6e02
Timothay <timothe.vanmansart@viacesi.fr>
2019-11-12 16:31:37 +0100

    post method

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -20,31 +20,47 @@ class SecurityController extends AbstractController
         $user = new Users();
 
         $form = $this->createForm(RegistrationType::class, $user);
+
+
             if ($request -> isMethod('GET')){
         $form->handleRequest($request);
         
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView()
         ]);
+            }
 
 
 
             if($request -> isMethod('POST')){
-        if($form->isSubmitted() && $form->isValid()) {
+                $form->handleRequest($request);
+            
+
+            if($form->isSubmitted()) {
+                $users= new Users;
+                $data = $form->getData();
+               /* $data['role_id'] = 1;
+                $nmdp = $data['password'];
+                */
+                $json_data = json_encode($data);
+
             $curl = curl_init();
 
-            curl_setopt($curl, CURLOPT-URL, 'localhost:8080/');
+            curl_setopt($curl, CURLOPT_URL, '10.97.184.127:27015/register-new-user');
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_COOKIESESSION, true);
             curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
 
             $postfields = array(
-                'center' => $centre,
-                'email' => $mail,
+                'center' => $center,
+                'email' => $email,
                 'password' => $password,
             );
+    
 
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $postfields);
+
+            
             $return = curl_exec($curl);
             curl_close($curl);
 
@@ -53,6 +69,6 @@ class SecurityController extends AbstractController
         
 
     }
-}
+            return $this->redirectToRoute("accueil");
         }
-    }
+}
