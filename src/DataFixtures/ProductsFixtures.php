84ad3b7fa605f93e14131c8fd4f6a7b825254fae
darksasukedu59 <44493087+darksasukedu59@users.noreply.github.com>
2019-11-13 14:53:43 +0100

    update boutique

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -5,15 +5,21 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Products;
+use App\Entity\Category;
 
 class ProductsFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+       
+        $category=new Category();
+        $category->setName("sweat");
+          $manager->persist($category);
         for ($i=1;$i<=10;$i++){
             $products=new Products();
             $products->setName("Produit n°$i")
-                     ->setPrice("$i");
+                     ->setPrice("$i")
+                     ->setCategory($category);
             $manager->persist($products);
         }
 
