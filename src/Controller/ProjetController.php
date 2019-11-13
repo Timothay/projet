86e51c24e802f457de053a36be9b09e7793fde03
naoki-bodiou <naoki.bodiou@viacesi.fr>
2019-11-13 10:10:54 +0100

    .

--- src/Controller/ProjetController.php
+++ src/Controller/ProjetController.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
 class ProjetController extends AbstractController
 {
     /**
@@ -16,72 +13,61 @@ class ProjetController extends AbstractController
             'controller_name' => 'ProjetController',
         ]);
     }
-
     /**
      * @Route("/calendrier", name="calendrier")
      */
     public function calendrier(){
         return $this->render('projet/calendrier.html.twig');
     }
-
     /**
      * @Route("/contact", name="contact")
      */
     public function contact(){
         return $this->render('projet/contact.html.twig');
     }
-
     /**
      * @Route("/boutique", name="boutique")
      */
     public function boutique(){
         return $this->render('projet/boutique.html.twig');
     }
-
     /**
      * @Route("/moncompte", name="moncompte")
      */
     public function moncompte(){
         return $this->render('projet/compte.html.twig');
     }
-
     /**
      * @Route("/admin", name="admin")
      */  
     public function admin(){
         return $this->render('projet/admin.html.twig');
     }
-
     /**
      * @Route("/connexion", name="connexion")
      */
     public function connexion() {
         return $this->render('projet/connexion.html.twig');
     }
-
  
-
     /**
      * @Route("/evenement/{id}", name="evenement")
      */
     public function evenement($id) {
         return $this->render('projet/evenement.html.twig');
     }
-
     /**
      * @Route("/panier", name="panier")
      */
     public function panier() {
         return $this->render('projet/panier.php');
     }
-
     /**
      * @Route("/ajouter", name="ajouter")
      */
     public function ajouter(){
         return $this->render('projet/ajouter.html.twig');
     }
-
     /**
      * @Route("/mentions", name="mentions")
      */
@@ -89,5 +75,3 @@ class ProjetController extends AbstractController
         return $this->render('projet/mentions.html.twig');
     }
 }
-
-
