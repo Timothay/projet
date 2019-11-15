c9b8573d7213608993537342f1fcc41f0cdcf8c9
Timothay <timothe.vanmansart@viacesi.fr>
2019-11-15 12:39:46 +0100

    Revert "Merge branch 'master' of https://github.com/Timothay/projet"
    This reverts commit 084c7de624bf3e299baebeaca5bcc6dfe798686d, reversing
    changes made to d0210046c6d644a9dcd513bbefe8ef8011bc773c.

--- src/Migrations/Version20191114151045.php
+++ src/Migrations/Version20191114151045.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191115102001 extends AbstractMigration
+final class Version20191114151045 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20191115102001 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE activities (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, date DATE NOT NULL, image VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE products ADD nb_ventes INT NOT NULL, ADD image VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE users ADD center VARCHAR(255) NOT NULL, ADD email VARCHAR(255) NOT NULL, DROP centre, DROP mail');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20191115102001 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE activities');
+        $this->addSql('ALTER TABLE products DROP nb_ventes, DROP image');
+        $this->addSql('ALTER TABLE users ADD centre VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD mail VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, DROP center, DROP email');
     }
 }
